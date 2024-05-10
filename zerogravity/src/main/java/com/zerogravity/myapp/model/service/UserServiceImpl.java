8214caab0a2e79daf4abcde90e2c13a05f788b27
minukHwang <damin2003@yonsei.ac.kr>
2024-05-10 10:55:13 +0900

    [ZRGV-240510] feat: Implement Mappers, Controllers, and Service layers; adjust schema

--- zerogravity/src/main/java/com/zerogravity/myapp/model/service/UserServiceImpl.java
+++ zerogravity/src/main/java/com/zerogravity/myapp/model/service/UserServiceImpl.java
@@ -1,5 +1,47 @@
 package com.zerogravity.myapp.model.service;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.zerogravity.myapp.model.dao.UserDao;
+import com.zerogravity.myapp.model.dto.User;
+import com.zerogravity.myapp.model.dto.UserInfo;
+
+@Service
+public class UserServiceImpl implements UserService {
+	private final UserDao userDao;
+	
+	@Autowired
+	public UserServiceImpl(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@Override
+	public User getUserByUserId(long userId) {
+		return userDao.selectUserByUserId(userId);
+	}
+
+	@Override
+	public UserInfo getUserInfoByUserId(long userId) {
+		return userDao.selectUserInfoByUserId(userId);
+	}
+
+	@Override
+	@Transactional
+	public boolean createUser(User user, UserInfo userInfo) {
+		int userResult = userDao.insertUser(user);
+		if(userResult == 1) {
+			int infoResult = userDao.insertUserInfo(userInfo);
+			return infoResult == 1;
+		}
+		return false;
+	}
+
+	@Override
+	@Transactional
+	public boolean removeUser(long userId) {
+		int result = userDao.deleteUser(userId);
+		return result == 1;
+	}
 }
