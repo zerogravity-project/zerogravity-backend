48cdd259b86584f39e8a3ac1044f82b60eba8e48
celestedayoung <dayoungpyo@gmail.com>
2024-05-12 01:53:56 +0900

    [ZRGV-240512] docs: Add Swagger API annotations

--- zerogravity/src/main/java/com/zerogravity/myapp/controller/DailyStaticsRestController.java
+++ zerogravity/src/main/java/com/zerogravity/myapp/controller/DailyStaticsRestController.java
@@ -14,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zerogravity.myapp.model.dto.DailyStatics;
 import com.zerogravity.myapp.model.service.DailyStaticsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api-zerogravity/statics")
+@Tag(name = "Daily Statics Management", description = "일일 감정 기록 API")
 public class DailyStaticsRestController {
 
     private final DailyStaticsService dailyStaticsService;
@@ -26,17 +33,27 @@ public class DailyStaticsRestController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<DailyStatics> getDailyStaticsByUserId(@PathVariable long userId) {
+    @Operation(summary = "일일 감정 기록 조회", description = "특정 사용자의 일일 감정 기록을 조회합니다.")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "사용자 일일 감정 기록을 찾음"),
+			@ApiResponse(responseCode = "404", description = "사용자 일일 감정 기록을 찾을 수 없음") 
+	})
+    public ResponseEntity<?> getDailyStaticsByUserId(@PathVariable long userId) {
         DailyStatics dailyStatics = dailyStaticsService.getDailyStaticsByUserId(userId);
         if (dailyStatics != null) {
-            return ResponseEntity.ok(dailyStatics);
+            return new ResponseEntity<>(dailyStatics, HttpStatus.OK);
         } else {
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping
-    public ResponseEntity<String> createDailyStatics(@RequestBody DailyStatics dailyStatics) {
+    @Operation(summary = "일일 감정 기록 생성", description = "특정 사용자의 일일 감정 기록을 생성합니다.")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "사용자 일일 감정 기록을 생성"),
+			@ApiResponse(responseCode = "404", description = "사용자 일일 감정 기록을 생성할 수 없음") 
+	})
+    public ResponseEntity<Void> createDailyStatics(@Parameter(description = "사용자 일일 감정 기록") @RequestBody DailyStatics dailyStatics) {
         boolean result = dailyStaticsService.createDailyStatics(dailyStatics);
         if (result) {
         	return new ResponseEntity<>(HttpStatus.CREATED);
@@ -46,12 +63,17 @@ public class DailyStaticsRestController {
     }
 
     @PutMapping
-    public ResponseEntity<String> modifyDailyStatics(@RequestBody DailyStatics dailyStatics) {
+    @Operation(summary = "일일 감정 기록 업데이트", description = "특정 사용자의 일일 감정 기록을 업데이트합니다.")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "사용자 일일 감정 기록을 업데이트"),
+			@ApiResponse(responseCode = "404", description = "사용자 일일 감정 기록을 업데이트할 수 없음") 
+	})
+    public ResponseEntity<Void> modifyDailyStatics(@Parameter(description = "사용자 일일 감정 기록") @RequestBody DailyStatics dailyStatics) {
         boolean result = dailyStaticsService.modifyDailyStatics(dailyStatics);
         if (result) {
-        	return new ResponseEntity<>(HttpStatus.OK);
+        	return new ResponseEntity<Void>(HttpStatus.OK);
         } else {
-        	return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        	return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
         }
     }
 }
