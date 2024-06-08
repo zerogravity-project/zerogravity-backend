9ec99259a19e2a517e112d8a0d9e09fb76e02e43
minukHwang <damin2003@yonsei.ac.kr>
2024-06-08 22:31:03 +0900

    [ZRGV-240608] feat: Add social login, logout, account deletion functionalities

--- zerogravity/src/main/java/com/zerogravity/myapp/controller/UserRestController.java
+++ zerogravity/src/main/java/com/zerogravity/myapp/controller/UserRestController.java
@@ -1,8 +1,11 @@
 package com.zerogravity.myapp.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zerogravity.myapp.model.dto.User;
 import com.zerogravity.myapp.model.dto.UserInfo;
 import com.zerogravity.myapp.model.service.UserService;
+import com.zerogravity.myapp.security.JWTUtil;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api-zerogravity/users")
@@ -27,33 +33,87 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class UserRestController {
 
 	private final UserService userService;
+	private final JWTUtil jwtUtil;
 
 	@Autowired
-	public UserRestController(UserService userService) {
+	public UserRestController(UserService userService, JWTUtil jwtUtil) {
 		this.userService = userService;
+		this.jwtUtil = jwtUtil;
 	}
 
-	@GetMapping("/{userId}")
+	@GetMapping("/me")
 	@Operation(summary = "사용자 정보 조회", description = "특정 사용자의 정보를 조회합니다.")
-	@ApiResponses(value = { 
-			@ApiResponse(responseCode = "200", description = "사용자 정보 찾음"),
-			@ApiResponse(responseCode = "404", description = "사용자 정보를 찾을 수 없음") 
-	})
-	public ResponseEntity<?> getUserByUserId(@PathVariable long userId) {
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "사용자 정보 찾음"),
+			@ApiResponse(responseCode = "204", description = "로그인 안됨"),
+			@ApiResponse(responseCode = "401", description = "인증 실패"),
+			@ApiResponse(responseCode = "404", description = "사용자 정보를 찾을 수 없음") })
+	public ResponseEntity<?> getProfile(@CookieValue(value = "token", required = false) String token) {
+		if (token == null) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+
+		if (jwtUtil.isExpired(token)) {
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		}
+
+		Long userId = jwtUtil.getUserId(token);
 		User user = userService.getUserByUserId(userId);
 		if (user != null) {
-			return new ResponseEntity<User>(user, HttpStatus.OK);
+			Map<String, Object> userProfile = Map.of("nickname", user.getNickname(), "profileImage",
+					user.getProfileImage(), "thumbnailImage", user.getThumbnailImage());
+			return new ResponseEntity<>(userProfile, HttpStatus.OK);
 		} else {
-			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
+	}
+
+	@PostMapping("/logout")
+	@Operation(summary = "로그아웃", description = "사용자를 로그아웃 시키고 JWT 쿠키를 삭제합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "로그아웃 성공"),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
+	public ResponseEntity<Void> logout(@CookieValue(value = "token", required = false) String token, HttpServletResponse response) {
+		if (token != null) {
+			Cookie jwtCookie = new Cookie("token", null);
+			jwtCookie.setHttpOnly(true);
+			jwtCookie.setPath("/");
+			jwtCookie.setMaxAge(0); // 쿠키를 즉시 만료
+			response.addCookie(jwtCookie);
+		}
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@DeleteMapping("/me")
+	@Operation(summary = "사용자 삭제", description = "현재 사용자를 삭제합니다.")
+	@ApiResponses({ @ApiResponse(responseCode = "204", description = "사용자 삭제 성공"),
+	    @ApiResponse(responseCode = "404", description = "사용자 정보를 찾을 수 없음") })
+	public ResponseEntity<Void> removeCurrentUser(@CookieValue(value = "token", required = false) String token, HttpServletResponse response) {
+	    if (token == null || jwtUtil.isExpired(token)) {
+	        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+	    }
+
+	    Long userId = jwtUtil.getUserId(token);
+	    boolean isDeleted = userService.removeUser(userId);
+	    if (isDeleted) {
+	        // JWT 쿠키 삭제
+	        Cookie jwtCookie = new Cookie("token", null);
+	        jwtCookie.setHttpOnly(true);
+	        jwtCookie.setPath("/");
+	        jwtCookie.setMaxAge(0); // 쿠키를 즉시 만료
+	        response.addCookie(jwtCookie);
+
+	        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    } else {
+	        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    }
 	}
 
 	@GetMapping("/{userId}/info")
 	@Operation(summary = "사용자 추가 정보 조회", description = "특정 사용자의 추가 정보를 조회합니다.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "사용자 추가 정보 찾음"),
-			@ApiResponse(responseCode = "404", description = "사용자 추가 정보를 찾을 수 없음") 
-	})
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "사용자 추가 정보 찾음"),
+			@ApiResponse(responseCode = "404", description = "사용자 추가 정보를 찾을 수 없음") })
 	public ResponseEntity<?> getUserInfoByUserId(@PathVariable long userId) {
 		UserInfo userInfo = userService.getUserInfoByUserId(userId);
 		if (userInfo != null) {
@@ -65,12 +125,10 @@ public class UserRestController {
 
 	@PostMapping
 	@Operation(summary = "새 사용자 생성", description = "새로운 사용자와 사용자 정보를 생성합니다.")
-	@ApiResponses(value = { 
-			@ApiResponse(responseCode = "201", description = "사용자 생성 성공"),
-			@ApiResponse(responseCode = "400", description = "잘못된 요청으로 인한 사용자 생성 실패") 
-	})
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "사용자 생성 성공"),
+			@ApiResponse(responseCode = "400", description = "잘못된 요청으로 인한 사용자 생성 실패") })
 	public ResponseEntity<Void> createUser(@Parameter(description = "사용자 정보") @RequestBody User user) {
-		if (isValidUser(user)){
+		if (isValidUser(user)) {
 			boolean isCreated = userService.createUser(user);
 			if (isCreated) {
 				return new ResponseEntity<>(HttpStatus.CREATED);
@@ -81,10 +139,8 @@ public class UserRestController {
 
 	@DeleteMapping("/{userId}")
 	@Operation(summary = "사용자 삭제", description = "특정 사용자를 삭제합니다.")
-    @ApiResponses({
-        @ApiResponse(responseCode = "204", description = "사용자 삭제 성공"),
-        @ApiResponse(responseCode = "404", description = "사용자 정보를 찾을 수 없음")
-    })
+	@ApiResponses({ @ApiResponse(responseCode = "204", description = "사용자 삭제 성공"),
+			@ApiResponse(responseCode = "404", description = "사용자 정보를 찾을 수 없음") })
 	public ResponseEntity<Void> removeUser(@PathVariable long userId) {
 		boolean isDeleted = userService.removeUser(userId);
 		if (isDeleted) {
@@ -95,7 +151,7 @@ public class UserRestController {
 	}
 
 	private boolean isValidUser(User user) {
-		return user != null && user.getEmail() != null && user.getName() != null;
+		return user != null;
 	}
 
 	private boolean isValidUserInfo(UserInfo userInfo) {
--- zerogravity/src/main/java/com/zerogravity/myapp/model/service/UserService.java
+++ zerogravity/src/main/java/com/zerogravity/myapp/model/service/UserService.java
@@ -6,14 +6,17 @@ import com.zerogravity.myapp.model.dto.UserInfo;
 public interface UserService {
 	// 사용자 정보 조회
 	public abstract User getUserByUserId(long userId);
-	
+
 	// 사용자 추가 정보 조회
 	public abstract UserInfo getUserInfoByUserId(long userId);
-	
+
 	// 사용자 정보 삽입
 	public abstract boolean createUser(User user);
-	
+
+	// 사용자 정보 업데이트
+	public abstract boolean modifyUser(User user);
+
 	// 사용자 정보 삭제
 	public abstract boolean removeUser(long userId);
-	
+
 }
